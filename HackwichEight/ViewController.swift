e9512eb3af2ac2020ee32ef3e87e833850f8a7a0
Jkresnak <cmstudent@uhwo-acm-01.uhwo.hawaii.edu>
2020-03-12 10:54:12 -1000

    Completed everything except for the Target Value Label

--- HackwichEight/ViewController.swift
+++ HackwichEight/ViewController.swift
@@ -12,35 +12,66 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var SlideText: UILabel!
     
-    @IBOutlet weak var OneHundredOne: UILabel!
-    
     @IBOutlet weak var OneHundredTwo: UILabel!
     
-    @IBOutlet weak var One: UILabel!
+    @IBOutlet weak var Zero: UILabel!
     
     @IBOutlet weak var Slider: UISlider!
     
-    var currentValue = 0
+    @IBOutlet weak var TargetValueLabel: UILabel!
     
+    var currentValue: Int = 0
+    
+    //HW8 Part 3 Part 2
    
+    var TargetValue: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
+        currentValue = lroundf(Slider.value)
+        TargetValue =  Int.random(in: 0...100)
+        
+        self.SlideText.text = "Get as close as possible to:"
+        self.OneHundredTwo.text = "100"
+        self.Zero.text = "0"
+        
     }
 
-        @IBAction func Button(_ sender: Any) {
-
-        let Alert = UIAlertController(title: "Hello World", message: message, preferredStyle:.alert)
+    @IBAction func Button(_ sender: Any) {
+    
+        let Message = "The current slider value is: \(currentValue)" + "\n Your Target value is: \(TargetValue)"
+            
+        let Alert = UIAlertController(title: "Hello World", message: Message, preferredStyle:.alert)
         
-        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        let action = UIAlertAction(title: "New Round", style: .default, handler: nil)
         
         Alert.addAction(action)
         
         present(Alert, animated: true, completion: nil)
+    
+        StartNewRound()
     }
+    
+    
     @IBAction func SliderMoved(_ sender: Any) {
         print("The value of the slider is:\(Slider.value)")
         currentValue = lroundf(Slider.value)
     }
+    
+    func StartNewRound(){
+    
+    TargetValue = Int.random(in: 0...100)
+    //currentValue = 1roundf(Slider.value)
+ 
+        //problem set 1
+        func updateTargetLabel(){
+    
+            self.TargetValueLabel.text = String(TargetValue)
+    
+        }
+    }
+    
 }
 
